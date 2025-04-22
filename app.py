fe063a53eee16100451802d7a0bdef7697a659a1
JoaoRizzieri <joaolucasrizzieri1@gmail.com>
2025-04-22 16:37:57 -0300

    teste commit

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 
 
-#Digite o primeiro número: 10  
-#Digite o segundo número: 0  
-#Digite a operação (+, -, *, /): /  
-#Erro: não é possível dividir por zero.
+# Digite o primeiro número: 10
+# Digite o segundo número: 0
+# Digite a operação (+, -, *, /): /
+# Erro: não é possível dividir por zero.
 
 numero1 = float(input("Digite o primeiro número: "))
 numero2 = float(input("Digite o segundo número: "))
-digite_a_operação = input("Digite a operação (+, -, *, /): ")   
+digite_a_operação = input("Digite a operação (+, -, *, /): ")
 
 if digite_a_operação == "+":
     resultado = numero1 + numero2
@@ -23,4 +23,5 @@ elif digite_a_operação == "/":
         resultado = numero1 / numero2
         print("Resultado: ", resultado)
     else:
-        print("Erro: seu idiota, vc é burro?.")    
+        print("Erro: seu idiota, vc é burro?.")
+        print("Erro: seu idiota, vc é burro?.")
